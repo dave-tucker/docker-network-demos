63662d714ea2d09246e053ae67c138012f9e70ba
Dave Tucker <dave@dtucker.co.uk>
2015-10-21 19:16:12 +0100

    Update multihost-local.sh
    Use 1.9.0-rc1 for all machines

--- multihost-local.sh
+++ multihost-local.sh
@@ -5,6 +5,7 @@ set -e
 # Docker Machine Setup
 docker-machine create \
 	-d virtualbox \
+	--virtualbox-boot2docker-url https://github.com/tianon/boot2docker-legacy/releases/download/v1.9.0-rc1/boot2docker.iso \
 	mhl-consul
 
 docker $(docker-machine config mhl-consul) run -d \
