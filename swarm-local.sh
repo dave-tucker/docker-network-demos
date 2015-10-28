01a1f53594a8abbc67992969ff70074e50889181
Dave Tucker <dave@dtucker.co.uk>
2015-10-28 14:26:09 -0700

    Add applescript for mh demo

--- swarm-local.sh
+++ swarm-local.sh
@@ -16,6 +16,7 @@ docker $(docker-machine config swl-consul) run -d \
 docker-machine create \
     -d virtualbox \
     --virtualbox-boot2docker-url https://github.com/tianon/boot2docker-legacy/releases/download/v1.9.0-rc3/boot2docker.iso \
+    --virtualbox-disk-size 50000 \
     --swarm \
     --swarm-image="swarm:1.0.0-rc2" \
     --swarm-master \
@@ -27,6 +28,7 @@ docker-machine create \
 docker-machine create \
     -d virtualbox \
     --virtualbox-boot2docker-url https://github.com/tianon/boot2docker-legacy/releases/download/v1.9.0-rc3/boot2docker.iso \
+    --virtualbox-disk-size 50000 \
     --swarm \
     --swarm-image="swarm:1.0.0-rc2" \
     --swarm-discovery="consul://$(docker-machine ip swl-consul):8500" \
